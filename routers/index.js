83bec8164cb79524069c5a8d61a3e5ba320b67e7
= <duynv@rikkeisoft.com>
2022-06-21 17:40:36 +0700

    update

--- routers/index.js
+++ routers/index.js
@@ -3,12 +3,16 @@ import conversation from './Conversations.js';
 import message from './messages.js';
 import user from './Users.js';
 import movie from './Movies.js';
+import express from 'express';
+
+let router = express.Router();
 
 function route(app) {
-    app.use('/conversation', conversation);
-    app.use('/message', message);
-    app.use('/account', user);
-    app.use('/film', movie);
+    router.use('/conversation', conversation);
+    router.use('/message', message);
+    router.use('/account', user);
+    router.use('/film', movie);
+    return app.use('/api-movie/', router)
 }
 
 export default route;
