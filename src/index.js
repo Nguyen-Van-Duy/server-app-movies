017a184eb419bb4d565e2e9b58d5a16f3e878754
= <duynv@rikkeisoft.com>
2022-07-29 09:08:09 +0700

    update 1

--- src/index.js
+++ src/index.js
@@ -32,6 +32,7 @@ app.use(allowCrossDomain);
 // app.use('/', posts)
 app.use(cors())
 route(app)
+app.use("lo", (req, res)=> res.json({id: 1}))
 connectSocket(server)
 mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => {
